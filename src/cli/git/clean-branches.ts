88547935ee2ece7012e65c73edc806c91267a3bd
Marviuz <marviuz.gaming@gmail.com>
2024-06-06 21:00:52 +0800

    feat(git): clean git branches

---
+++ src/cli/git/clean-branches.ts
@@ -0,0 +1,77 @@
+import {
+  cancel,
+  confirm,
+  group,
+  intro,
+  log,
+  multiselect,
+  outro,
+  spinner,
+} from '@clack/prompts';
+import { Command } from 'commander';
+import $git from 'simple-git';
+
+export const clean = new Command()
+  .name('clean-branches')
+  .description('Delete selected loca branches')
+  .action(async () => {
+    intro('Git helpers');
+    const git = $git();
+
+    const localBranches = Object.entries((await git.branchLocal()).branches);
+
+    const branches = localBranches
+      .filter(([_, details]) => !details.current)
+      .map(([branch, details]) => ({
+        label: branch,
+        value: branch,
+        hint: details.label,
+      }));
+
+    const { selectedBranches, confirmation } = await group(
+      {
+        selectedBranches: () =>
+          multiselect({
+            message: 'Local branches',
+            options: branches,
+            initialValues: branches.map(({ value }) => value),
+          }),
+        confirmation: () =>
+          confirm({
+            message:
+              'Are you sure you want to delete the selected branches forcefully?',
+          }),
+      },
+      {
+        onCancel: () => {
+          cancel('Operation cancelled');
+          process.exit(0);
+        },
+      },
+    );
+
+    if (!confirmation) {
+      cancel('Operation cancelled.');
+      process.exit(0);
+    }
+    const s = spinner();
+
+    s.start('Forcefully deleting branches...');
+
+    const attemptedBranches = Object.entries(
+      (await git.deleteLocalBranches(selectedBranches)).branches,
+    );
+
+    attemptedBranches.forEach(([branch, details]) => {
+      if (details.success) {
+        log.success(branch);
+        return;
+      }
+
+      log.error(`${branch} failed!`);
+    });
+
+    s.stop();
+
+    outro("You're all set!");
+  });
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,13 @@
 import { Command } from 'commander';
 import pkg from '@/../package.json';
 import { media } from './cli/media';
+import { git } from './cli/git';
 
 const program = new Command()
   .name(pkg.name)
   .description(pkg.description)
   .version(pkg.version);
 
-program.addCommand(media);
+program.addCommand(media).addCommand(git);
 
 program.parse();
