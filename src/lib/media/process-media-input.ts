ff3c7f1f1fdb98fff93616ee6a0a90c183de1386
Marviuz <marviuz.gaming@gmail.com>
2025-02-01 15:59:28 +0800

    chore: remove media history command

--- src/lib/media/process-media-input.ts
+++ src/lib/media/process-media-input.ts
@@ -1,5 +1,4 @@
 import { spinner } from '@clack/prompts';
-import { MediaHistoryController } from '@/db/controllers/media-history-controller';
 import { handleErrors } from '@/utils/error-handler';
 import { exec } from '@/utils/exec';
 
@@ -8,9 +7,6 @@ export async function processMediaInput(input: string, output: string) {
   try {
     s.start('Processing...');
     await exec(`ffmpeg -i "${input}" -c copy "${output}"`);
-
-    s.message('Updating cache...');
-    await MediaHistoryController.create({ from: input, to: output });
 
     s.stop('Done');
   } catch (err) {
